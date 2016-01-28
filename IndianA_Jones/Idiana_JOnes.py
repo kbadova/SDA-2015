a21511cbd2c369cf189fda0baaa84966474e487d
Krasimira Badova <krasi_b2@abv.bg>
2016-01-28 14:49:59 +0200

    changes

--- IndianA_Jones/Idiana_JOnes.py
+++ IndianA_Jones/Idiana_JOnes.py
@@ -95,49 +95,15 @@ class IndianaJones:
 
     def __init__(self):
         cli = CLI()
-        # self.list = []
         self.tuple = cli.input()
-        print(self.tuple)
         self.N = cli.get_N()
-        # self.list_of_carried_items = []
-
-    # def dict_to_list(self):
-    #     for item in self.dict:
-    #         self.list.append(self.dict[item])
-    #     self.list = sorted(self.list)
-    #     self.list.reverse()
-    #     return self.list
-
-    # def get_value_from_dict(self, value):
-    #     for item in self.dict:
-    #         if self.dict[item] == value:
-    #             return item
-
-    # def carry_items(self):
-    #     self.dict_to_list()
-    #     for tuplee in self.list:
-    #         if tuplee[1] <= self.N:
-    #             self.list_of_carried_items.append(self.get_value_from_dict(tuplee))
-    #             self.N -= tuplee[1]
-    #     return self.list_of_carried_items
 
     def totalvalue(self, comb):
-        ' Totalise a particular combination of items'
         totwt = totval = 0
         for item, wt, val in comb:
             totwt += wt
             totval += val
-        return (totval, -totwt) if totwt <= 400 else (0, 0)
-
-    # items = (
-                        #     ("map", 9, 150), ("compass", 13, 35), ("water", 153, 200), ("sandwich", 50, 160),
-                        #     ("glucose", 15, 60), ("tin", 68, 45), ("banana", 27, 60), ("apple", 39, 40),
-                        #     ("cheese", 23, 30), ("beer", 52, 10), ("suntan cream", 11, 70), ("camera", 32, 30),
-                        #     ("t-shirt", 24, 15), ("trousers", 48, 10), ("umbrella", 73, 40),
-                        #     ("waterproof trousers", 42, 70), ("waterproof overclothes", 43, 75),
-                        #     ("note-case", 22, 80), ("sunglasses", 7, 20), ("towel", 18, 12),
-                        #     ("socks", 4, 50), ("book", 30, 10),
-                        #     )
+        return (totval, -totwt) if totwt <= self.N else (0, 0)
 
     def message_carry_items(self, result):
         return ("Indiana JOnes can carry the following items\n  " +
@@ -154,10 +120,16 @@ class IndianaJones:
 
 
 def main():
-    # test = CLI()
-    # print(test.input())
     indi = IndianaJones()
     print(indi.carry_items())
+
+    # items = (("map", 9, 150), ("compass", 13, 35), ("water", 153, 200), ("sandwich", 50, 160),
+        #     ("glucose", 15, 60), ("tin", 68, 45), ("banana", 27, 60), ("apple", 39, 40),
+        #     ("cheese", 23, 30), ("beer", 52, 10), ("suntan cream", 11, 70), ("camera", 32, 30),
+        #     ("t-shirt", 24, 15), ("trousers", 48, 10), ("umbrella", 73, 40),
+        #     ("waterproof trousers", 42, 70), ("waterproof overclothes", 43, 75),
+        #     ("note-case", 22, 80), ("sunglasses", 7, 20), ("towel", 18, 12),
+        #     ("socks", 4, 50), ("book", 30, 10),)
 
 if __name__ == '__main__':
     main()
